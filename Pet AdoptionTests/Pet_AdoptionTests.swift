03a638b7b2c92f66c61ae38a22c656062db7b0a9
Sam Armstrong <sear234@uky.edu>
2019-09-12 14:22:41 -0400

    Initial Commit

---
+++ Pet AdoptionTests/Pet_AdoptionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pet_AdoptionTests.swift
+//  Pet AdoptionTests
+//
+//  Created by Sam Armstrong on 9/12/19.
+//  Copyright © 2019 Crime Scene Investigators. All rights reserved.
+//
+
+import XCTest
+@testable import Pet_Adoption
+
+class Pet_AdoptionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
