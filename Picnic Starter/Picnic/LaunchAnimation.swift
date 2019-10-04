cf74117a661244dfa1b92ee7367c41f7f14ff106
Sam Armstrong <armstrongsam25@gmail.com>
2019-10-04 17:23:55 -0400

    Fixed issue where launch screen was throwing errors on button press for next screen

--- Picnic Starter/Picnic/LaunchAnimation.swift
+++ Picnic Starter/Picnic/LaunchAnimation.swift
@@ -6,22 +6,21 @@
 //  Copyright © 2019 Ray Wenderlich. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 import UIKit
 
 class LaunchAnimation: UIViewController {
+  
+  @IBOutlet weak var tongue: UIImageView!
     
-    @IBOutlet weak var tongue: UIImageView!
-    
-  override func viewDidAppear(_ animated: Bool) {
+  /*override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-
-//    performSegue(withIdentifier: "CoolEffect", sender: nil)
-  }
+    //performSegue(withIdentifier: "CoolEffect", sender: nil)
+  }*/
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    UIView.animate(withDuration: 2.0, delay: 0.0, options: [], animations: {
+    UIView.animate(withDuration: 1.0, delay: 0.0, options: [], animations: {
       self.tongue.alpha = 1.0
       self.tongue.center = CGPoint(x: 122, y: 553)
     }/*, completion: { finished in
